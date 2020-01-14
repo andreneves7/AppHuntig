c012aafbad34734028ef0dbd90a4a171b65da5ce
andreneves7 <jatsmneves@gmail.com>
2020-01-14 03:48:36 +0000

    lista de grupos por user admin aparecem e clicar nele passa para activity diferent

--- app/src/main/java/com/example/app/GrupoActivity.kt
+++ app/src/main/java/com/example/app/GrupoActivity.kt
@@ -3,18 +3,10 @@ package com.example.app
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.ExpandableListAdapter
-import android.widget.ExpandableListView
-import android.widget.TextView
-import android.widget.Toast
-import androidx.core.content.ContextCompat.startActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.android.synthetic.main.activity_expandable.*
-import java.util.HashMap
+import kotlinx.android.synthetic.main.activity_grupo.*
 
 
 class GrupoActivity : AppCompatActivity() {
@@ -29,7 +21,7 @@ class GrupoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_expandable)
+        setContentView(R.layout.activity_grupo)
 
 
         val evento = bEvento
@@ -38,22 +30,6 @@ class GrupoActivity : AppCompatActivity() {
             startActivity(Intent (this, EventoActivity :: class.java ))
         }
 
-
-
-        val diff_pedidos : MutableList<String> = ArrayList()
-        diff_pedidos.add("indian Oceans")
-
-        val diff_membros : MutableList<String> = ArrayList()
-        diff_membros.add("Asia")
-
-        title.add("Pedidos")
-        title.add("Membros")
-
-        subTitle.add(diff_pedidos)
-        subTitle.add(diff_membros)
-
-
-        expandableListView.setAdapter(ExpandableListViewAdapter(this,expandableListView,title,subTitle))
 
 
     }
--- app/src/main/java/com/example/app/VerGrupoActivity.kt
+++ app/src/main/java/com/example/app/VerGrupoActivity.kt
@@ -1,25 +1,38 @@
 package com.example.app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.ViewParent
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.android.synthetic.main.activity_expandable.*
+import kotlinx.android.synthetic.main.activity_grupo.*
 import kotlinx.android.synthetic.main.activity_ver_grupo.*
+import org.jetbrains.anko.listView
+import java.text.FieldPosition
 
 class VerGrupoActivity : AppCompatActivity() {
 
     val mAuth = FirebaseFirestore.getInstance()
     val Auth = FirebaseAuth.getInstance()
 
-    val title: MutableList<String> = ArrayList()
-    val subTitle: MutableList<MutableList<String>> = ArrayList()
+
+
+//    val title: MutableList<String> = ArrayList()
+//    val subTitle: MutableList<MutableList<String>> = ArrayList()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ver_grupo)
 
+
+        val list = ListView2
 
         val user = Auth.currentUser
         if (user != null) {
@@ -28,45 +41,84 @@ class VerGrupoActivity : AppCompatActivity() {
                 if (document != null) {
 
 
-//                    val ref = document.data?.get("grupo")as List<String>
-//                    for (grupo in ref){
-//                        mAuth.collection("Grupos").document(grupo).get().addOnSuccessListener {
-//                            result ->
-//                            val grupoDoc = result
-//                            values.add(grupoDoc!!.get("nome").toString())
-//                        }
-//                    }
+                    val values = ArrayList<String>()
 
 
-                    val g = document.data?.get("grupo").toString()
+                    val ref = document.data?.get("grupo") as List<String>
+                    Log.d(
+                        "VerGrupo",
+                        " ref $ref"
+                    )
 
-                    val diff_grupos: MutableList<String> = ArrayList()
-                    diff_grupos.add(g)
+                    if (ref.isEmpty()) {
+                        Log.d(
+                            "VerGrupo",
+                            " sem grupos deste user"
+                        )
+                    } else {
+                        Log.d(
+                            "VerGrupo",
+                            " grupos deste user"
+                        )
 
-                    subTitle.add(diff_grupos)
+
+                        for (grupo in ref) {
+                            Log.d(
+                                "VerGrupo",
+                                "grupo  $grupo"
+                            )
+
+
+                            var d = values.add(grupo).toString()
+                            Log.d(
+                                "VerGrupo",
+                                "values $d"
+                            )
+
+                            val adapter = ArrayAdapter(this,R.layout.listview_item, values)
+
+                            list.setAdapter(adapter)
+                            
+                            list.onItemClickListener = object : AdapterView.OnItemClickListener {
+
+
+
+                                override fun onItemClick(parent: AdapterView<*>, view: View,
+                                                         position: Int, id: Long){
+
+                                    val itemValue = list.getItemAtPosition(position) as String
+
+                                    val intent = Intent(view.context,  GrupoActivity:: class.java )
+                                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                                    startActivity(intent)
+
+                                    Toast.makeText(applicationContext,
+                                        "Position :$position\nItem Value : $itemValue", Toast.LENGTH_LONG)
+                                        .show()
+
+
+                                }
+
+                            }
+
+                            
+                        }
+                    }
 
                     Log.d(
-                        "Grupo",
+                        "VerGrupo",
                         " ${document.id} => ${document.data?.get("name")}, ${document.data?.get("grupo")}"
                     )
 
 
-
-
-                    Log.d("evento","DocumentSnapshot data: ${document.data?.get("name")}"
+                    Log.d(
+                        "VerGrupo", "DocumentSnapshot data: ${document.data?.get("name")}"
                     )
                 } else {
-                    Log.d("evento", "No such document")
+                    Log.d("VerGrupo", "No such document")
                 }
             }
         }
-
-
-        title.add("Grupos")
-
-        expandableListView2.setAdapter(ExpandableListViewAdapter(this,expandableListView2,title,subTitle))
-
-
 
     }
 }
