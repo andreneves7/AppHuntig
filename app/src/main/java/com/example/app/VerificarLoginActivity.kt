f1be7324f306c07bfbae05bdbbd1754b279e5ee1
jatsm <jatsmneves@gmail.com>
2020-09-12 02:34:02 +0100

    verificaçao de login corrigida para diferentes entradas , login esta sem vericaçao de email

--- app/src/main/java/com/example/app/VerificarLoginActivity.kt
+++ app/src/main/java/com/example/app/VerificarLoginActivity.kt
@@ -3,18 +3,45 @@ package com.example.app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import org.jetbrains.anko.startActivity
+import android.content.Intent
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.core.view.isVisible
+
 
 
 class VerificarLoginActivity : AppCompatActivity() {
 
+        val a = FirebaseAuth.getInstance().currentUser
+        val b = FirebaseFirestore.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val c = b.collection("Users").document(a?.uid.toString())
+            if (a == null) {
+                startActivity<LoginActivity>()
+            } else {
 
-        if (FirebaseAuth.getInstance().currentUser == null) {
-            startActivity<LoginActivity>()
-        } else {
-            startActivity<FiltrosActivity>()
+
+                c.get().addOnSuccessListener { document ->
+                if (document != null) {
+                    val org = document.data?.get("Org")
+
+                    if (org == false){
+
+                        startActivity<FiltrosActivity>()
+                    }
+                    else{
+                        startActivity<OrgActivity>()
+                    }
+                }
+            }
         }
         finish()
     }
